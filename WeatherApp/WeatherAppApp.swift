b705b0c38665fbc52a723ac190af86b34ea6ab43
Vivek Singh <viveksingh9301@gmail.com>
2025-10-02 17:10:02 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vivek Singh on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
