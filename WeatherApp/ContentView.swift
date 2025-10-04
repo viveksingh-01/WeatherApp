b8bcb0fdf82974bfe8bf323f854eec3d9ed877a7
Vivek Singh <viveksingh9301@gmail.com>
2025-10-04 14:57:23 +0530

    Replace blue background with a linear gradient

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.blue)
-                .ignoresSafeArea()
+            LinearGradient(colors: [Color.blue, Color.white],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea()
         }
     }
 }
